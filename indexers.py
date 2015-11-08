e82c2bdf629c941148ab144a267efbdc6d73002d
Fengda HUANG <h@phodal.com>
2015-11-08 19:45:20 +0800

    add basic query

--- indexers.py
+++ indexers.py
@@ -1,11 +1,12 @@
-"""
-Basic indexing and searching example adapted from http://lucene.apache.org/core/4_10_1/core/index.html
-"""
+#!/usr/bin/python2.6
+# -*- coding: utf-8 -*-
 
 import lucene
 from java.io import File
 from org.apache.lucene import analysis, document, index, queryparser, search, store, util
 from lupyne import engine
+import csv
+
 lucene.initVM()
 
 # # # lucene # # #
@@ -13,44 +14,36 @@ lucene.initVM()
 analyzer = analysis.standard.StandardAnalyzer(util.Version.LUCENE_CURRENT)
 
 # Store the index in memory:
-directory = store.RAMDirectory()
+# directory = store.RAMDirectory()
 # To store an index on disk, use this instead:
 # Directory directory = FSDirectory.open(File("/tmp/testindex"))
 directory = store.FSDirectory.open(File("./testindex"))
 config = index.IndexWriterConfig(util.Version.LUCENE_CURRENT, analyzer)
 iwriter = index.IndexWriter(directory, config)
-doc = document.Document()
-text = "This is the text to be indexed."
-doc.add(document.Field("fieldname", text, document.TextField.TYPE_STORED))
-iwriter.addDocument(doc)
+with open('data/xiaoyou.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        name = row['姓名']
+        sex = row['性别']
+        year = row['毕业年份']
+        doc = document.Document()
+        doc.add(document.Field("name", name, document.TextField.TYPE_STORED))
+        doc.add(document.Field("sex", sex, document.TextField.TYPE_STORED))
+        doc.add(document.Field("year", year, document.TextField.TYPE_STORED))
+        iwriter.addDocument(doc)
 iwriter.close()
 
 # Now search the index:
 ireader = index.IndexReader.open(directory)
 isearcher = search.IndexSearcher(ireader)
 # Parse a simple query that searches for "text":
-parser = queryparser.classic.QueryParser(util.Version.LUCENE_CURRENT, "fieldname", analyzer)
-query = parser.parse("text")
+parser = queryparser.classic.QueryParser(util.Version.LUCENE_CURRENT, "name", analyzer)
+query = parser.parse("张")
 hits = isearcher.search(query, None, 1000).scoreDocs
-assert len(hits) == 1
+print len(hits)
 # Iterate through the results:
-for hit in hits:
-    hitDoc = isearcher.doc(hit.doc)
-    assert hitDoc['fieldname'] == text
+# for hit in hits:
+#     hitDoc = isearcher.doc(hit.doc)
+#     assert hitDoc['fieldname'] == text
 ireader.close()
 directory.close()
-
-# # # lupyne # # #
-
-# Store the index in memory:
-indexer = engine.Indexer()              # Indexer combines Writer and Searcher; RAMDirectory and StandardAnalyzer are defaults
-indexer.set('fieldname', stored=True)   # settings for all documents of indexer; indexed and tokenized is the default
-indexer.add(fieldname=text)             # add document
-indexer.commit()                        # commit changes and refresh searcher
-
-# Now search the index:
-hits = indexer.search('text', field='fieldname')    # parsing handled if necessary
-assert len(hits) == 1
-for hit in hits:                                    # hits support mapping interface
-    assert hit['fieldname'] == text
-# closing is handled automatically
