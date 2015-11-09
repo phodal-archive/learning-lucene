fc6dfb4fd74e2b826ca25ef476beb44593b143d6
Fengda HUANG <h@phodal.com>
2015-11-09 17:24:55 +0800

    use to location

--- indexers.py
+++ indexers.py
@@ -10,8 +10,6 @@ from lupyne.engine import Query
 
 lucene.initVM()
 
-# # # lucene # # #
-
 analyzer = analysis.standard.StandardAnalyzer(util.Version.LUCENE_CURRENT)
 
 # Store the index in memory:
@@ -20,39 +18,36 @@ analyzer = analysis.standard.StandardAnalyzer(util.Version.LUCENE_CURRENT)
 # Directory directory = FSDirectory.open(File("/tmp/testindex"))
 directory = store.FSDirectory.open(File("./testindex"))
 config = index.IndexWriterConfig(util.Version.LUCENE_CURRENT, analyzer)
-iwriter = index.IndexWriter(directory, config)
-with open('data/xiaoyou.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        name = row['姓名']
-        sex = row['性别']
-        year = row['毕业年份']
-        phone = row['联系电话']
-        tec = row['QQ/微信']
-        email = row['Email']
-        office = row['工作单位']
-        doc = document.Document()
-        doc.add(document.Field("name", name, document.TextField.TYPE_STORED))
-        doc.add(document.Field("sex", sex, document.TextField.TYPE_STORED))
-        doc.add(document.Field("year", year, document.TextField.TYPE_STORED))
-        doc.add(document.Field("phone", phone, document.TextField.TYPE_STORED))
-        doc.add(document.Field("tec", tec, document.TextField.TYPE_STORED))
-        doc.add(document.Field("email", email, document.TextField.TYPE_STORED))
-        doc.add(document.Field("office", office, document.TextField.TYPE_STORED))
-        iwriter.addDocument(doc)
-iwriter.close()
+
+def create_index():
+    iwriter = index.IndexWriter(directory, config)
+    with open('data/location.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            province = row['province']
+            city = row['city']
+            zone = row['zone']
+            tone = row['tone']
+            lng = row['lng']
+            lat = row['lat']
+            doc = document.Document()
+            doc.add(document.Field("province", province, document.TextField.TYPE_STORED))
+            doc.add(document.Field("city", city, document.TextField.TYPE_STORED))
+            doc.add(document.Field("zone", zone, document.TextField.TYPE_STORED))
+            doc.add(document.Field("tone", tone, document.TextField.TYPE_STORED))
+            doc.add(document.Field("lng", lng, document.TextField.TYPE_STORED))
+            doc.add(document.Field("lat", lat, document.TextField.TYPE_STORED))
+            iwriter.addDocument(doc)
+    iwriter.close()
+
+# create_index()
 
 # Now search the index:
 ireader = index.IndexReader.open(directory)
 isearcher = search.IndexSearcher(ireader)
 # Parse a simple query that searches for "text":
-# parser = queryparser.classic.QueryParser(util.Version.LUCENE_CURRENT, "name", analyzer)
-# query = parser.parse("赵钱孙李周吴郑王")
-# query = search.TermQuery(index.Term('name', "search.TermQuery(index.Term('text', 'lucene'))"))
-# query= search.TermQuery(index.Term('sex', '女'))
-
-query = search.PhraseQuery()
-query.add(index.Term('name', '赵'))
+parser = queryparser.classic.QueryParser(util.Version.LUCENE_CURRENT, "tone", analyzer)
+query = parser.parse("电子城街道")
 
 hits = isearcher.search(query, None, 1000).scoreDocs
 # print hits.doc
@@ -60,7 +55,6 @@ hits = isearcher.search(query, None, 1000).scoreDocs
 
 for hit in hits:
     hitDoc = isearcher.doc(hit.doc)
-    if hitDoc['office'] :
-        print hitDoc['name'], hitDoc['sex'], hitDoc['tec'], hitDoc['email']
+    print hitDoc['province'], hitDoc['city'], hitDoc['zone'], hitDoc['tone']
 ireader.close()
 directory.close()
